d1d065ed164ce6a2180dbcde0d4c8f1967d8c94f
Miguel Colque <micky.cm@outlook.es>
2025-06-28 10:32:33 -0400

    Corregida la visualización de imágenes en las tarjetas de establecimientos

--- foodtrail_project/core/urls.py
+++ foodtrail_project/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- foodtrail_project/core/views.py
+++ foodtrail_project/core/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render
 from establishments.models import Establishment, EstablishmentType, MealType
 from menu.models import MenuItemType
 
+from urllib.parse import urlencode
+from django.conf import settings
+
 def home(request):
     # Obtener todos los tipos para los filtros
     establishment_types = EstablishmentType.objects.all()
     meal_types = MealType.objects.all()
     menu_item_types = MenuItemType.objects.all()
     
-    # Obtener parámetros de filtro
-    establishment_type_filter = request.GET.get('establishment_type')
-    meal_type_filter = request.GET.get('meal_type')
-    menu_item_type_filter = request.GET.get('menu_item_type')
+    # Obtener parámetros de filtro actuales
+    current_params = request.GET.copy()
+    establishment_type_filter = current_params.get('establishment_type')
+    meal_type_filter = current_params.get('meal_type')
+    menu_item_type_filter = current_params.get('menu_item_type')
     
     # Filtrar establecimientos
     establishments = Establishment.objects.all()
@@ -27,6 +31,16 @@ def home(request):
             menu_items__menu_item_types__slug=menu_item_type_filter
         ).distinct()
     
+    # Función para construir URLs con múltiples parámetros
+    def build_url(params_to_update):
+        params = current_params.copy()
+        for key, value in params_to_update.items():
+            if value is None:
+                params.pop(key, None)
+            else:
+                params[key] = value
+        return f"?{urlencode(params, doseq=True)}" if params else "?"
+    
     context = {
         'establishments': establishments,
         'establishment_types': establishment_types,
@@ -36,6 +50,8 @@ def home(request):
             'establishment_type': establishment_type_filter,
             'meal_type': meal_type_filter,
             'menu_item_type': menu_item_type_filter,
-        }
+        },
+        'build_url': build_url,
+        'MEDIA_URL': settings.MEDIA_URL
     }
     return render(request, 'core/home.html', context)
